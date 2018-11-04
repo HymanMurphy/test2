1bf0dd08d44ef9fd750f6b88124775cd103e75f6
hyman <hymanwise@gmail.com>
2018-11-04 04:18:00 -0800

    practice

--- venv/comon/no1.py
+++ venv/comon/no1.py
@@ -1,2 +1,16 @@
-import numpy
-print("hello world!")
+class student(object):
+    def __init__(self, name):
+        self.name = name
+    def __call__(self):
+        print("may")
+        print(self.name)
+    def __len__(self):
+        return len(self.name)
+p = student("hiououou")
+p()
+print(len(p))
+
+class Test():
+    def __len__(self):
+        return 0
+    print(bool(Test()))
