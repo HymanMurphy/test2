6a1af1825ad94e71cd217616ed4ea9202dd47c03
hyman <hymanwise@gmail.com>
2018-11-10 22:41:03 -0800

    firslt

--- venv/comon/opencv.py
+++ venv/comon/opencv.py
@@ -1,2 +1,20 @@
-import cv2
-print("hello workd")
+from random import choice
+
+class Person:
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return "hello {name}".format(name=self.name)
+
+def main():
+    people = [
+        Person('jame'),
+        Person('bom')
+    ]
+    person = choice(people)
+    print(person)
+
+if __name__ == '__main__':
+    main()
